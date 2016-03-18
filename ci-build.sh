91e92f56491a159b254237bd058e3225a4141727
Qian Hong <qhong@codeweavers.com>
2016-03-18 17:22:53 +0800

    disable log.

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 
 cd src
 make wm_names.t
@@ -10,12 +9,10 @@ make
 bin/mintty.exe --help
 bin/mintty.exe --version
 mintty.exe --log mintty.old.log --exec echo hello mintty
-strace -o mintty.strace.txt -f bin/mintty.exe --log mintty.new.log --exec echo hello mintty
+#strace -o mintty.strace.txt -f bin/mintty.exe --log mintty.new.log --exec echo hello mintty
+bin/mintty.exe --exec echo hello mintty
 ls -al
 pwd
 ps aux
 cat mintty.old.log
 grep hello mintty.old.log
-cat mintty.new.log
-grep hello mintty.new.log
-cat mintty.strace.txt
